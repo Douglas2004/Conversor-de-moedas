cf52e8d22bc62a10d9cafddde3943ac007e18bb1
Douglas Alves Sousa <douglas_s2004@hotmail.com>
2017-11-20 18:03:03 -0200

    Add files via upload

---
+++ ex009.py
@@ -0,0 +1,6 @@
+moeda = float(input('quanto dinheiro você tem na carteira? R$'))
+dolar = float(3.27)
+
+converte =  moeda / dolar
+
+print('com R${} você consegui comprar ${} dolares'.format(moeda, converte))
